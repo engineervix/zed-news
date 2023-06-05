735df943c9ace432bd84125cb95b813c6ca61b85
Victor Miti <victormiti@umusebo.com>
2023-06-05 20:48:16 +0100

    [CF-Pages-Skip] chore: improve intro wording

--- app/core/podcast/content.py
+++ app/core/podcast/content.py
@@ -36,12 +36,12 @@ async def update_article_with_summary(title: str, url: HttpUrl, date: datetime.d
 async def random_opening():
     episode_number = num2words(await get_episode_number(), to="ordinal")
     data = [
-        f"Today is {today_human_readable}. Welcome to the {episode_number} edition of the Zed News Podcast — I'm your friendly host, {podcast_host}, and I'm thrilled to have you join us on this journey of exploring the latest news and stories from across Zambia.",
-        f"Today is {today_human_readable}. Welcome to the {episode_number} installment of the Zed News Podcast! I'm {podcast_host}, your amiable host, and I'm excited to have you accompany us as we embark on a voyage through the latest news and stories from across Zambia.",
-        f"Greetings and a warm welcome to the {episode_number} edition of the Zed News Podcast! It's {today_human_readable}, and I'm your genial host, {podcast_host}. Join us as we dive into the dynamic world of news and uncover the intriguing narratives shaping Zambia's landscape.",
-        f"We're thrilled to have you here on this {today_human_readable}, for the {episode_number} edition of the Zed News Podcast! I'm {podcast_host}, your affable host, and together, let's embark on an enriching journey through the vibrant tapestry of news and stories that define Zambia.",
-        f"Welcome, welcome! It's a pleasure to have you join us today for the {episode_number} installment of the Zed News Podcast! I'm {podcast_host}, your friendly guide through the ever-evolving news landscape of Zambia. Get ready to immerse yourself in the latest headlines and captivating narratives that await us.",
-        f"Here we are, on {today_human_readable}, marking the {episode_number} edition of the Zed News Podcast! I'm {podcast_host}, your enthusiastic host, and I'm delighted to have you with us as we traverse the vast expanse of news and stories that illuminate the heart of Zambia.",
+        f"Today is {today_human_readable}. Welcome to the {episode_number} edition of the Zed News Podcast — I'm your friendly host, {podcast_host}, and I'm thrilled to have you join me on this journey of exploring the latest news and stories from across Zambia.",
+        f"Today is {today_human_readable}. Welcome to the {episode_number} installment of the Zed News Podcast! I'm your host, {podcast_host}, and I'm excited to have you accompany me as we embark on a voyage through the latest news and stories from across Zambia.",
+        f"Greetings and a warm welcome to the {episode_number} edition of the Zed News Podcast! It's {today_human_readable}, and I'm your host, {podcast_host}. Join me as we dive into the dynamic world of news and uncover the intriguing narratives shaping Zambia's landscape.",
+        f"It's {today_human_readable}. I am thrilled to have you here for the {episode_number} edition of the Zed News Podcast! This is your host, {podcast_host}. Together, let's embark on an enriching journey through the vibrant tapestry of news and stories that define Zambia.",
+        f"Welcome! It's a pleasure to have you join me today for the {episode_number} installment of the Zed News Podcast! I'm {podcast_host}, your friendly guide through the ever-evolving news landscape of Zambia. Get ready to immerse yourself in the latest headlines and captivating narratives that await us.",
+        f"Here we are, on {today_human_readable}, marking the {episode_number} edition of the Zed News Podcast! I'm {podcast_host}, your enthusiastic host, and I'm delighted to have you with me as we traverse the vast expanse of news and stories that illuminate the heart of Zambia.",
     ]
 
     return random.choice(data)
@@ -52,7 +52,7 @@ def random_intro():
         "Whether you're commuting, relaxing at home, or going about your day, the Zed News Podcast is designed to keep you informed and engaged. We know your time is valuable, so we'll deliver the news in a concise yet captivating format, allowing you to stay updated without feeling overwhelmed.",
         "In today's episode, we'll dive into the headlines making waves across Zambia. Sit back, relax, and let's explore the stories that shape our nation.",
         "As we delve into the news landscape, we'll bring you a curated selection of the most important stories from various sources. Stay tuned for a brief overview of what's happening in Zambia.",
-        "Join us on this informative journey as we uncover the latest developments and provide you with a snapshot of the events happening around Zambia and beyond.",
+        "Join me on this informative journey as we uncover the latest developments and provide you with a snapshot of the events happening around Zambia and beyond.",
         "Our automated curation process scours the web to compile the most relevant news stories just for you. Get ready for a concise yet comprehensive rundown of the news buzzing in Zambia.",
         "With our advanced algorithms, we've handpicked the top stories that matter. From breaking news to compelling features, we'll keep you up to date with the pulse of Zambia.",
     ]
@@ -76,7 +76,7 @@ def random_dig_in():
 def random_outro():
     variations = [
         f"And that, dear listeners, brings us to the end of another fantastic edition of the 'Zed News Podcast'. I hope you enjoyed our time together today, catching up on the latest happenings. Until next time, this is {podcast_host} signing off, wishing you a wonderful day or night ahead. Take care, stay safe, and keep being awesome. Later!",
-        f"And with that, we come to the conclusion of another captivating edition of the 'Zed News Podcast'. I hope you found our exploration of the news landscape insightful and illuminating. Until we meet again, this is {podcast_host} bidding you farewell, wishing you a remarkable day or night. Stay informed, and keep making a difference. Goodbye, everyone!",
+        f"And with that, we come to the conclusion of another captivating edition of the 'Zed News Podcast'. I hope you found our exploration of the news landscape insightful and illuminating. Until we meet again, this is {podcast_host} bidding you farewell, wishing you a remarkable day or night. Stay informed, and keep making a difference. Goodbye!",
         f"That brings us to the end of this remarkable episode of the 'Zed News Podcast'. I trust you found our discussion enlightening and thought-provoking. Until next time, this is {podcast_host}, your host, signing off. Take care and see you later!",
         f"And with that, we wrap up another exciting edition of the 'Zed News Podcast'. I hope you enjoyed our time together, staying up to date with the latest news. Until we reconvene, this is {podcast_host}, your friendly voice in the news, saying farewell. Bye for now!",
         f"That concludes our journey through this edition of the 'Zed News Podcast'. I trust you found our exploration of the news landscape insightful and illuminating. Until our paths cross again, this is {podcast_host}, your guide in the realm of information, bidding you adieu. Bye bye for now!",
