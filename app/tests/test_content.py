a50255433f595fe16eb212a38ca59b3ca614f98d
Victor Miti <victormiti@umusebo.com>
2023-06-24 11:24:29 +0100

    test: test transcript creation

--- app/tests/test_content.py
+++ app/tests/test_content.py
@@ -1,12 +1,16 @@
 import asyncio
 import datetime
+import os
+import shutil
+import tempfile
 import unittest
-from unittest.mock import AsyncMock, patch
+from unittest.mock import call, patch
 
 from tortoise import Tortoise
 
 from app.core.db.models import Article
 from app.core.podcast.content import (
+    create_transcript,
     get_episode_number,
     random_dig_in,
     random_intro,
@@ -14,6 +18,7 @@ from app.core.podcast.content import (
     random_outro,
     update_article_with_summary,
 )
+from app.core.utilities import today_human_readable
 
 
 class MockEpisodeQuerySet:
@@ -117,6 +122,123 @@ class TestRandomContent(unittest.TestCase):
         result = random_outro()
         self.assertIsInstance(result, str)
         self.assertNotEqual(result, "")
+
+
+class TestTranscriptCreation(unittest.IsolatedAsyncioTestCase):
+    async def asyncSetUp(self):
+        self.temp_dir = tempfile.mkdtemp()
+        self.text_file = os.path.join(self.temp_dir, "transcript_test.txt")
+
+        # Set up the test database connection
+        await Tortoise.init(
+            db_url="sqlite://:memory:",  # SQLite in-memory database
+            modules={"models": ["app.core.db.models"]},
+        )
+        await Tortoise.generate_schemas(safe=True)
+
+        self.article_data = [
+            {
+                "source": "Source 1",
+                "url": "https://example.com/news-1",
+                "title": "News 1",
+                "content": "Content 1",
+                "category": "",
+            },
+            {
+                "source": "Source 2",
+                "url": "https://website.com/news-2",
+                "title": "News 2",
+                "content": "Content 2",
+                "category": "",
+            },
+            {
+                "source": "Source 2",
+                "url": "https://website.com/news-3",
+                "title": "News 3",
+                "content": "Content 3",
+                "category": "",
+            },
+            {
+                "source": "Source 3",
+                "url": "https://anothersite.co.zm/news-4",
+                "title": "News 4",
+                "content": "Content 4",
+                "category": "Sports",
+            },
+        ]
+
+        # add more articles so that Source 3 has 10 articles
+        for i in range(len(self.article_data) + 1, 14):
+            self.article_data.append(
+                {
+                    "source": "Source 3",
+                    "url": f"https://anothersite.co.zm/news-{i}",
+                    "title": f"News {i}",
+                    "content": f"Content {i}",
+                    "category": "Sports",
+                }
+            )
+
+        for data in self.article_data:
+            await Article(**data).save()
+
+    async def asyncTearDown(self):
+        shutil.rmtree(self.temp_dir)
+
+        await Tortoise.close_connections()
+        await Tortoise._drop_databases()
+
+    @patch("app.core.podcast.content.get_episode_number")
+    @patch("app.core.summarization.backends.openai.summarize")
+    def test_create_transcript_with_openai(self, mock_summarizer, mock_get_episode_number):
+        mock_get_episode_number.return_value = 22
+        summary = "This is a summary of the content"
+        mock_summarizer.return_value = summary
+
+        destination = self.text_file
+        news = self.article_data
+
+        asyncio.run(create_transcript(news, destination, mock_summarizer))
+
+        with open(destination, "r") as file:
+            transcript_content = file.read()
+
+        expected_calls = [call(item["content"], item["title"]) for item in news]
+
+        mock_summarizer.assert_has_calls(expected_calls)
+        self.assertIn(summary, transcript_content)
+        self.assertEqual(len(news), transcript_content.count(summary))
+        self.assertIn("We are going to start with news from", transcript_content)
+        self.assertIn("Next up, we have news from", transcript_content)
+        self.assertIn("To wrap up today's edition", transcript_content)
+        self.assertIn(today_human_readable, transcript_content)
+        self.assertIn("twenty-second", transcript_content)
+
+    @patch("app.core.podcast.content.get_episode_number")
+    @patch("app.core.summarization.backends.cohere.summarize")
+    def test_create_transcript_with_cohere(self, mock_summarizer, mock_get_episode_number):
+        mock_get_episode_number.return_value = 11
+        summary = "This is a summary of the content"
+        mock_summarizer.return_value = summary
+
+        destination = self.text_file
+        news = self.article_data
+
+        asyncio.run(create_transcript(news, destination, mock_summarizer))
+
+        with open(destination, "r") as file:
+            transcript_content = file.read()
+
+        expected_calls = [call(item["content"], item["title"]) for item in news]
+
+        mock_summarizer.assert_has_calls(expected_calls)
+        self.assertIn(summary, transcript_content)
+        self.assertEqual(len(news), transcript_content.count(summary))
+        self.assertIn("We are going to start with news from", transcript_content)
+        self.assertIn("Next up, we have news from", transcript_content)
+        self.assertIn("To wrap up today's edition", transcript_content)
+        self.assertIn(today_human_readable, transcript_content)
+        self.assertIn("eleventh", transcript_content)
 
 
 if __name__ == "__main__":
