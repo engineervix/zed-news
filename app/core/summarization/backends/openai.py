94754b87479771d652acde136e00988ba3673018
Victor Miti <victormiti@umusebo.com>
2023-07-10 21:41:31 +0100

    fix: quick and dirty fix for exceeding token limit

--- app/core/summarization/backends/openai.py
+++ app/core/summarization/backends/openai.py
@@ -1,10 +1,12 @@
 import logging
+import textwrap
 
 from langchain import OpenAI, PromptTemplate
 
 from app.core.utilities import OPENAI_API_KEY
 
 llm = OpenAI(temperature=0, openai_api_key=OPENAI_API_KEY)
+MAX_TOKENS = 4096
 
 
 def summarize(content: str, title: str) -> str:
@@ -17,7 +19,18 @@ def summarize(content: str, title: str) -> str:
     {entry}
     """
 
+    # Calculate the maximum number of tokens available for the prompt
+    max_prompt_tokens = MAX_TOKENS - llm.get_num_tokens(template)
+
+    # Trim the content if it exceeds the available tokens
+    # TODO: Instead of truncating the content, split it
+    # see <https://python.langchain.com/docs/modules/data_connection/document_transformers/text_splitters/split_by_token>
+    max_chars = max_prompt_tokens * 4  # Assuming 1 token ≈ 4 chars
+    if len(content) > max_chars:
+        content = textwrap.shorten(content, width=max_chars, placeholder=" ...")
+
     prompt = PromptTemplate(input_variables=["entry"], template=template)
+
     summary_prompt = prompt.format(entry=content)
 
     num_tokens = llm.get_num_tokens(summary_prompt)
