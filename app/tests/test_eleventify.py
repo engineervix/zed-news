d077ff5c2cb7b7d625b3244f4982bb65f2886ab1
Victor Miti <victormiti@umusebo.com>
2024-02-21 18:16:54 +0000

    test: fix broken test

--- app/tests/test_eleventify.py
+++ app/tests/test_eleventify.py
@@ -19,9 +19,13 @@ class TestEleventify(unittest.TestCase):
     def setUp(self):
         self.temp_dir = tempfile.mkdtemp()
         self.dist_file = os.path.join(self.temp_dir, f"{today_iso_fmt}.njk")
+        self.news_headlines = os.path.join(self.temp_dir, f"{today_iso_fmt}_news_headlines.txt")
 
-        self.patcher = patch("app.core.podcast.eleventify.dist_file", self.dist_file)
-        self.patcher.start()
+        self.patch_dist_file = patch("app.core.podcast.eleventify.dist_file", self.dist_file)
+        self.patch_news_headlines = patch("app.core.podcast.eleventify.news_headlines", self.news_headlines)
+
+        self.patch_dist_file.start()
+        self.patch_news_headlines.start()
 
         # Bind model classes to test db. Since we have a complete list of
         # all models, we do not need to recursively bind dependencies.
@@ -83,10 +87,20 @@ class TestEleventify(unittest.TestCase):
         # If we wanted, we could re-bind the models to their original
         # database here. But for tests this is probably not necessary.
 
-        self.patcher.stop()
+        self.patch_dist_file.stop()
+        self.patch_news_headlines.stop()
 
+    @patch("app.core.podcast.eleventify.get_content")
+    @patch("app.core.podcast.eleventify.create_episode_summary")
     @patch("app.core.podcast.eleventify.logging")
-    def test_render_jinja_template(self, mock_logging):
+    def test_render_jinja_template(self, mock_logging, mock_episode_summary, mock_get_content):
+        mock_episode_summary.return_value = (
+            "In today's news, we cover various topics, including the Lorem Ipsum incident, "
+            "where a prankster replaced all traffic signs with memes, a study suggesting that "
+            "talking to houseplants improves Wi-Fi signal strength, and an interview with a "
+            "local squirrel who claims to have invented a new dance craze sweeping the forest floor."
+        )
+        mock_get_content.return_value = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
         render_jinja_template(production_time=120, word_count=5000)
 
         mock_logging.info.assert_called_once_with("Rendering Jinja template ...")
@@ -106,7 +120,7 @@ class TestEleventify(unittest.TestCase):
             self.assertNotIn(f"Test Source {i}", content)
             self.assertNotIn(f"https://example.com/{i}", content)
 
-        self.assertIn(today_human_readable, content)
+        self.assertIn("including the Lorem Ipsum incident", content)
         self.assertIn('episode: "001"', content)
         self.assertIn(podcast_host, content)
         self.assertIn(lingo, content)
