da0d6753c6fa4a4c66f142047b9ad86173cd2e9a
Victor Miti <victormiti@umusebo.com>
2024-09-03 14:27:29 +0100

    refactor: use Mixtral-8x22B-Instruct-v0.1 for article summarisation

--- app/core/summarization/backends/together.py
+++ app/core/summarization/backends/together.py
@@ -16,7 +16,7 @@ def summarize(content: str, title: str) -> str:
     """
 
     prompt = f"You are a distinguished news editor and content publisher, your task is to summarize the following news entry. The summary should accurately reflect the main message and arguments presented in the original news entry, while also being concise and easy to understand. Your summary should not exceed two sentences.\n\n ```{content}```:"
-    model = "mistralai/Mixtral-8x7B-v0.1"
+    model = "Mixtral-8x22B-Instruct-v0.1"
     temperature = 0.7
     max_tokens = 192
 
@@ -29,7 +29,16 @@ def summarize(content: str, title: str) -> str:
     time.sleep(1.5)
     logging.info(response)
 
-    return response.choices[0].text
+    if result := response.choices[0].text.strip():
+        result = result.replace("```", "")  # Remove triple backticks
+        first_line = result.splitlines()[0].lower()
+        unwanted = ["summary:", "here's", "here is", "sure"]
+
+        if any(string in first_line for string in unwanted):
+            # Remove the first line from result
+            result = "\n".join(result.split("\n")[1:])
+
+        return result.replace("\n", "")  # Remove newlines
 
 
 def brief_summary(content: str, title: str) -> str:
